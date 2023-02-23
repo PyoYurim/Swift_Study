f083808f8b262a152a0a72bc1708876532b6f38d
PyoYurim <pyoqhfk1225@naver.com>
2023-02-23 18:45:49 +0900

    ImgaeViewer

--- ImageViewer/ImageViewer/ViewController.swift
+++ ImageViewer/ImageViewer/ViewController.swift
@@ -8,12 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var numImage = 1
+    var maxImage = 6
+    
 
+    @IBOutlet var imgView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        imgView.image = UIImage(named: "1.png")
     }
 
-
+    @IBAction func btnBeforeImage(_ sender: UIButton) {
+        numImage = numImage - 1
+        if (numImage < 1) {
+            numImage = maxImage
+        }
+        let imageName = String(numImage) + ".png"
+        imgView.image = UIImage(named: imageName)
+    }
+    
+    @IBAction func btnAfterImage(_ sender: UIButton) {
+        numImage = numImage + 1
+        if (numImage > maxImage) {
+            numImage = 1
+        }
+        let imageName = String(numImage) + ".png"
+        imgView.image = UIImage(named: imageName)
+    }
 }
+
 
