308d97d128a69d00d155221949884b239906ae2d
PyoYurim <pyoqhfk1225@naver.com>
2023-03-17 23:07:00 +0900

    SpotifyLogin

---
+++ SpotifyLoginSampleApp/SpotifyLoginSampleApp/EnterEmailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  EnterEmailViewController.swift
+//  SpotifyLoginSampleApp
+//
+//  Created by 표유림 on 2023/03/17.
+//
+
+import UIKit
+
+class EnterEmailViewController: UIViewController {
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        //Navigation Bar 보이기
+        navigationController?.navigationBar.isHidden = false
+    }
+    
+}
---
+++ SpotifyLoginSampleApp/SpotifyLoginSampleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpotifyLoginSampleApp
+//
+//  Created by 표유림 on 2023/03/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
